5d60b95977c0e4f48c18af6d02d2b9143eb3b44b
Lum7n <56406099+Lum7n@users.noreply.github.com>
2020-10-31 00:47:49 +0100

    Add files via upload

---
+++ Aufgaben/L03_FormElements/script.ts
@@ -0,0 +1,33 @@
+namespace Hexenkessel {
+
+    window.addEventListener("load", function (): void {
+        
+        init();
+
+        window.setTimeout(setMain, 1200);
+
+    });
+
+    function setMain(): void {
+        var w: HTMLElement = <HTMLElement>document.getElementById("startWindow");
+        w.style.display = "none";
+        var b: HTMLElement = <HTMLElement>document.body;
+        b.style.backgroundColor = "#ce9f73";
+        b.style.overflowY = "auto";
+        var m: HTMLElement = <HTMLElement>document.getElementById("main");
+        m.style.display = "inherit";
+    }
+
+    function init(): void {
+        console.log("love");
+    }
+
+
+
+
+
+
+
+
+
+}
