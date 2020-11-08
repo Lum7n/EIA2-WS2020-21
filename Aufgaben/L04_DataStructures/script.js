77f1a9786a41cf2115f7b1fd7b7cc626f7c03865
Lum7n <56406099+Lum7n@users.noreply.github.com>
2020-11-08 18:24:15 +0100

    Add files via upload

--- Aufgaben/L04_DataStructures/script.js
+++ Aufgaben/L04_DataStructures/script.js
@@ -1,10 +1,20 @@
 "use strict";
-var Hexenkessel;
-(function (Hexenkessel) {
+var Hexenkessel_L04;
+(function (Hexenkessel_L04) {
     window.addEventListener("load", function () {
         init();
         window.setTimeout(setMain, 1200);
     });
+    function init() {
+        console.log("no_love");
+        let potionName = document.getElementById("name");
+        potionName.addEventListener("input", displayPotion);
+        let potionDescription = document.getElementById("description");
+        potionDescription.addEventListener("input", displayPotion);
+        let potionRisks = document.getElementById("risks");
+        potionRisks.addEventListener("input", displayPotion);
+        console.log("love");
+    }
     function setMain() {
         var w = document.getElementById("startWindow");
         w.style.display = "none";
@@ -14,8 +24,27 @@ var Hexenkessel;
         var m = document.getElementById("main");
         m.style.display = "inherit";
     }
-    function init() {
-        console.log("love");
+    function displayPotion(_event) {
+        let target = _event.target;
+        if (target.name == "name") {
+            let potion = target.value;
+            console.log(potion);
+            let name = document.getElementById("potionName");
+            name.innerHTML = "Name: " + potion;
+        }
+        else if (target.name == "description") {
+            let potionDes = target.value;
+            let description = document.getElementById("potionDescription");
+            description.innerHTML = "Beschreibung: " + potionDes;
+        }
+        else if (target.name == "risks") {
+            let potionRisk = target.value;
+            let risks = document.getElementById("potionRisks");
+            risks.innerHTML = "Risiken und Nebenwirkungen: " + potionRisk;
+        }
+        else {
+            console.log("noInput");
+        }
     }
-})(Hexenkessel || (Hexenkessel = {}));
+})(Hexenkessel_L04 || (Hexenkessel_L04 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgaben/L04_DataStructures/script.ts
+++ Aufgaben/L04_DataStructures/script.ts
@@ -1,12 +1,31 @@
 namespace Hexenkessel_L04 {
 
     window.addEventListener("load", function (): void {
-        
+
         init();
 
         window.setTimeout(setMain, 1200);
 
     });
+
+
+
+
+    function init(): void {
+        console.log("no_love");
+
+        let potionName: HTMLElement = <HTMLElement>document.getElementById("name");
+        potionName.addEventListener("input", displayPotion);
+
+        let potionDescription: HTMLElement = <HTMLElement>document.getElementById("description");
+        potionDescription.addEventListener("input", displayPotion);
+
+        let potionRisks: HTMLElement = <HTMLElement>document.getElementById("risks");
+        potionRisks.addEventListener("input", displayPotion);
+
+        console.log("love");
+
+    }
 
     function setMain(): void {
         var w: HTMLElement = <HTMLElement>document.getElementById("startWindow");
@@ -18,14 +37,29 @@ namespace Hexenkessel_L04 {
         m.style.display = "inherit";
     }
 
-    function init(): void {
-        console.log("love");
+    function displayPotion(_event: Event): void {
+        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+
+        if (target.name == "name") {
+            let potion: string = target.value;
+            console.log(potion);
+            let name: HTMLElement = <HTMLElement>document.getElementById("potionName");
+            name.innerHTML = "Name: " + potion;
+
+        } else if (target.name == "description") {
+            let potionDes: string = target.value;
+            let description: HTMLElement = <HTMLElement>document.getElementById("potionDescription");
+            description.innerHTML = "Beschreibung: " + potionDes;
+
+        } else if (target.name == "risks") {
+            let potionRisk: string = target.value;
+            let risks: HTMLElement = <HTMLElement>document.getElementById("potionRisks");
+            risks.innerHTML = "Risiken und Nebenwirkungen: " + potionRisk;
+
+        } else {
+            console.log("noInput");
+        }
     }
-
-
-
-
-
 
 
 
